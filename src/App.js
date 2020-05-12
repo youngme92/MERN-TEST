1825ea77f409a31f74dd207ea72dedde3fb9e1d1
youngme92 <youngme992@gmail.com>
2020-05-12 16:38:04 +0900

    create-react-app express merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const useUsername = () => {
+  const [username, setUsername] = useState("");
+ useEffect(() => {
+   fetch('http://localhost:3001/api').then(res => res.json()).then(data=> setUsername(data.username))
+ }, [])
+ return {username}
+}
+
 function App() {
+  const {username} = useUsername();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{username ? `Hello ${username}` : `Hello world`}</h1>      
     </div>
   );
 }
