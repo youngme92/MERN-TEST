271c6c6b71b4917f531de7d2375b498efb1c060a
youngme92 <youngme992@gmail.com>
2020-05-12 17:05:55 +0900

    server client both startsetup

--- servers/server.js
+++ servers/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
