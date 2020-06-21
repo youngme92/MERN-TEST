0527a75b8d6af4e2d14cabf265b67d46a48ec905
youngme92 <youngme992@gmail.com>
2020-06-21 17:14:46 +0900

    Deploying heroku

--- servers/server.js
+++ servers/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
 // const cors = require('cors'); 기본세팅에서 지워짐
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT ? process.env.PORT : 3000
 
+// mongoose setting
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/react-express",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 // app.use(cors()); 기본세팅에서 지워짐
+// bodyParser setting
+app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
